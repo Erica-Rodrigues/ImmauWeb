bdafb5358aa3518d8315a918e2a02f16a498ee01
Erica <erica.soso@hotmail.com>
2025-10-22 12:53:40 +0200

    création de la page d'accueil

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AccueilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
