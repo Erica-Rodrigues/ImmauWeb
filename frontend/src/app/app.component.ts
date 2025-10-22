94d8eb983fd5ec44863ca2d3ff81b31f6724fcdf
Erica <erica.soso@hotmail.com>
2025-10-22 21:03:55 +0200

    création de la barre de navigation + lien vers l'accueil

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AccueilComponent } from './pages/accueil/accueil.component';
+import { NavigationComponent } from "./composant/navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,AccueilComponent],
+  imports: [RouterOutlet, AccueilComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
