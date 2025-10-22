806058f1d05253784380e4d7caae41db5f114e94
Erica <erica.soso@hotmail.com>
2025-10-22 22:27:43 +0200

    création du footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AccueilComponent } from './pages/accueil/accueil.component';
 import { NavigationComponent } from "./composant/navigation/navigation.component";
+import { FooterComponent } from "./composant/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AccueilComponent, NavigationComponent],
+  imports: [RouterOutlet, AccueilComponent, NavigationComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
