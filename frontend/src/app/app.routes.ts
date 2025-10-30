9edfbac56811f7625f71d84740ac312ca7dfc3f5
Erica <erica.soso@hotmail.com>
2025-10-30 11:31:24 +0100

    création du composant location + création de la route vers la page location

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AccueilComponent } from './pages/accueil/accueil.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { LocationComponent } from './pages/location/location.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'accueil',pathMatch:'full'},
@@ -10,4 +11,5 @@ export const routes: Routes = [
     {path:'login',component:LoginComponent},
     {path:'register',component:RegisterComponent},
     {path: 'contact', component:ContactComponent},
+    {path: 'location', component:LocationComponent},
 ];
